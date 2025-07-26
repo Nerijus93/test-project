b62cb59b91896771d6eec28fea3a06e5c90801db
Nerijus93 <nerijus.kmitas@gmail.com>
2025-07-26 16:49:10 +0100

    Update javaScript.js

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,16 +1,27 @@
-/* Page 1 JavaScript
-Home Page
+/* Page 5 JavaScript
+Statistics page
 made by: Nerijus Kmitas 
 Student ID x24170232 */
 
-// Get the reference to the quiz button by button ID
-const quizButton = document.getElementById('quizButton');
+//Get references to slider input and display elements
+const slider = document.getElementById("rangeSlider"); //Element to show Range Slider
+const sliderValue = document.getElementById("sliderValue"); //Element to show the percentage
+const sliderText = document.getElementById("sliderText"); //Text to display people fed
 
-// Check or the button exists on the page
-if (quizButton) {
-  // Adding event listener to the quiz button
-  quizButton.addEventListener('click', function () {
-    //Log the message in the browser when button is clicked
-    console.log('Quiz button clicked! Navigating to Page 2.');
+//To make sure that all slider elements are present on the page
+if (slider && sliderValue && sliderText) {
+  //Add an "input" event listener so this so this will run when user moves slider
+  slider.addEventListener("input", () => {
+    //Convert slider value String into integer value
+    const value = parseInt(slider.value);
+
+    // Update the visible percentage value with the current value
+    sliderValue.textContent = `${value}%`;
+
+    // Calculate how many people could be fed (based on max 3.45 billion)
+    const estimatedPeopleFed = Math.floor((value / 100) * 3450); // Result will be displayed in millions
+
+    // Update the text below the slider
+    sliderText.textContent = `We could feed over ${estimatedPeopleFed.toLocaleString()} million people annually`;
   });
 }
